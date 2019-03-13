cc692215b8a7e6bcf96456231790e5aba2bcdbd8
mks <maks.bezrodnyi@gmail.com>
2019-03-13 14:24:12 +0200

    new commit

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 function authenticate (login, password) {
-    if (password === 'password') {
+    if (password === 'password' && login === 'login') {
       return 'You were logged in';
     } else {
       return 'Login is incorrect';
