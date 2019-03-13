00111299f76c5a7a38258a39ac4aa4fa2af6940e
mks <maks.bezrodnyi@gmail.com>
2019-03-13 14:02:22 +0200

    change auth

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 function authenticate (login, password) {
-    if (login === 'login' || password === 'password') {
+    if (password === 'password') {
       return 'You were logged in';
     } else {
       return 'Login is incorrect';
